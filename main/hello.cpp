257571d7fdee39771983bc39cb61fbb582b8e142
Akhil1205 <93047553+Akhil1205@users.noreply.github.com>
2023-02-17 16:27:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built \n";
+  cout << "I have successfull  y built \n";
   return 0;
 }
