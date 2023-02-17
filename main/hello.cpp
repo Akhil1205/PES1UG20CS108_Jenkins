be8237f50f318184b72a62d3177417efe7bfb2aa
Akhil1205 <93047553+Akhil1205@users.noreply.github.com>
2023-02-17 16:05:30 +0530

    Create hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built it \n";
+  cout << "I have successfully built \n";
   return 0;
 }
